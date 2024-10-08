515ff5c68590d6d1c6dc29a63481d738eca9cbe6
Pascal Allen <pascal.allen88@gmail.com>
2024-10-08 16:44:38 -0500

    fix linter error

--- web/app/src/pages/IndexPage.tsx
+++ web/app/src/pages/IndexPage.tsx
@@ -26,7 +26,7 @@ const IndexPage = (): ReactElement => {
                   <h5 className="card-title">Card title</h5>
                   <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
                   <p className="card-text">
-                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                    Some quick example text to build on the card title and make up the bulk of the card&#39;s content.
                   </p>
                   <a href="#" className="card-link">
                     Card link
@@ -43,7 +43,7 @@ const IndexPage = (): ReactElement => {
                   <h5 className="card-title">Card title</h5>
                   <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
                   <p className="card-text">
-                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                    Some quick example text to build on the card title and make up the bulk of the card&#39;s content.
                   </p>
                   <a href="#" className="card-link">
                     Card link
