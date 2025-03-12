d6bcfa3ab2a925811da90694ede3210eb22b2474
Pascal Allen <pascal.allen88@gmail.com>
2025-03-11 19:42:00 -0500

    persist school-user association when a school admin adds a user

--- internal/carline/application/command/user.go
+++ internal/carline/application/command/user.go
@@ -5,11 +5,12 @@ import (
 )
 
 type RegisterUser struct {
-	Id           ulid.ULID `json:"id"`
-	FirstName    string    `json:"first_name"`
-	LastName     string    `json:"last_name"`
-	EmailAddress string    `json:"email_address"`
-	Admin        bool      `json:"admin"`
+	Id           ulid.ULID  `json:"id"`
+	SchoolId     *ulid.ULID `json:"school_id,omitempty"`
+	FirstName    string     `json:"first_name"`
+	LastName     string     `json:"last_name"`
+	EmailAddress string     `json:"email_address"`
+	Admin        bool       `json:"admin"`
 }
 
 func (c RegisterUser) CommandName() string {
@@ -17,10 +18,10 @@ func (c RegisterUser) CommandName() string {
 }
 
 type UpdateUser struct {
-	Id           ulid.ULID
-	FirstName    string
-	LastName     string
-	EmailAddress string
+	Id           ulid.ULID `json:"id"`
+	FirstName    string    `json:"first_name"`
+	LastName     string    `json:"last_name"`
+	EmailAddress string    `json:"email_address"`
 }
 
 func (c UpdateUser) CommandName() string {
